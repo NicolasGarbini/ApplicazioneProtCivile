c099c2478cf8835faa220c0296424a0925e89724
NicolasGarbini <st10569@iismarconipieralisi.it>
2025-02-28 18:51:16 +0100

    ConsegnaFinale

--- app/src/main/java/com/example/applicazioneprotcivile/Login.java
+++ app/src/main/java/com/example/applicazioneprotcivile/Login.java
@@ -1,6 +1,9 @@
 package com.example.applicazioneprotcivile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnEffettuaAccesso = findViewById(R.id.btn_effettuaAccesso);
+        btnEffettuaAccesso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
 
     }

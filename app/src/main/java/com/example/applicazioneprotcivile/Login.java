a16980d2fd41bed68b84dcd89e3f6f2add79bccc
NicolasGarbini <st10569@iismarconipieralisi.it>
2025-03-07 11:26:38 +0100

    lezione 07/03/2025

--- app/src/main/java/com/example/applicazioneprotcivile/Login.java
+++ app/src/main/java/com/example/applicazioneprotcivile/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +33,18 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+        ScrollView scrollView = findViewById(R.id.sv_login);
+        // Scroll to the bottom
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+
 
     }
 }
